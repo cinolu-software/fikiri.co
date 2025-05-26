fbacc91576a036d0b969e9e1181ceb4a4c47bb3b
Wilfried Musanzi <musanziw.m@gmail.com>
2025-05-26 15:39:16 +0200

    refactor: simplify by removing unused methods and imports

--- src/app/landing/data-access/calls.service.ts
+++ src/app/landing/data-access/calls.service.ts
@@ -1,36 +1,17 @@
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { IApplication, ICall } from '../../shared/utils/types/models.type';
-import { Router } from '@angular/router';
+import { ICall } from '../../shared/utils/types/models.type';
 import { APIService } from '../../shared/services/api/api.service';
 import { IAPIResponse } from '../../shared/services/api/types/api-response.type';
-import { ToastrService } from '../../shared/services/toast/toastr.service';
 import { buildQueryParams } from '../../shared/utils/helpers/build-query-params.fn';
 import { QueryParams } from '../utils/types/query-params.type';
 
 @Injectable()
-export class CallService {
+export class CallsService {
   #apiService = inject(APIService);
-  #toast = inject(ToastrService);
-  #router = inject(Router);
-
-  getOne(id: string): Observable<IAPIResponse<ICall>> {
-    return this.#apiService.get(`calls/${id}`);
-  }
 
   getPublished(queryParams: QueryParams): Observable<IAPIResponse<[ICall[], number]>> {
     const params = buildQueryParams(queryParams as unknown as Record<string, string>);
     return this.#apiService.get('calls/find-published', params);
-  }
-
-  apply(call: string | undefined, responses: JSON): Observable<IAPIResponse<IApplication>> {
-    const onSuccess = async () => {
-      this.#toast.showSuccess('Candidature soumise');
-      this.#router.navigate(['/calls']);
-    };
-    const onError = async (error: string) => {
-      this.#toast.showError(error);
-    };
-    return this.#apiService.post('solutions', { call, responses }, onSuccess, onError);
   }
 }
