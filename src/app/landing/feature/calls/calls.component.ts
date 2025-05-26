0653bc2cc8b6c9b595e5afbbe8690d17a4bb9ffb
Wilfried Musanzi <musanziw.m@gmail.com>
2025-05-26 15:40:28 +0200

    feat: implement calls component with pagination and image gallery

--- src/app/landing/feature/calls/calls.component.ts
+++ src/app/landing/feature/calls/calls.component.ts
@@ -1,8 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { CallsService } from '../../data-access/calls.service';
+import { Observable } from 'rxjs';
+import { IAPIResponse } from '../../../shared/services/api/types/api-response.type';
+import { ICall } from '../../../shared/utils/types/models.type';
+import { QueryParams } from '../../utils/types/query-params.type';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { GalleriaModule } from 'primeng/galleria';
+import { environment } from '../../../../environments/environment';
+import { ApiImgPipe } from '../../../shared/pipes/api-img.pipe';
+import { NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'app-calls',
-  imports: [],
+  providers: [CallsService],
+  imports: [CommonModule, NgIcon, RouterLink, NgxPaginationModule, GalleriaModule, ApiImgPipe],
   templateUrl: './calls.component.html',
 })
-export class CallsComponent {}
+export class CallsComponent implements OnInit {
+  #callsService = inject(CallsService);
+  #router = inject(Router);
+  calls$: Observable<IAPIResponse<[ICall[], number]>> | undefined;
+  imgUrl = `${environment.apiUrl}uploads/calls/`;
+  queryParams = signal<QueryParams>({
+    page: this.#router.routerState.snapshot.root.queryParams['page'] || null,
+  });
+  responsiveOptions = [
+    { breakpoint: '1300px', numVisible: 4 },
+    { breakpoint: '575px', numVisible: 2 },
+  ];
+
+  ngOnInit(): void {
+    this.calls$ = this.#callsService.getPublished(this.queryParams());
+  }
+
+  onPageChange(currentPage: number): void {
+    this.queryParams().page = currentPage === 1 ? null : currentPage;
+    this.updateRouteAndSolutions();
+  }
+
+  updateRoute(): void {
+    const { page } = this.queryParams();
+    const queryParams = { page };
+    this.#router.navigate(['/'], { queryParams });
+  }
+
+  loadCalls(): void {
+    this.calls$ = this.#callsService.getPublished(this.queryParams());
+  }
+
+  updateRouteAndSolutions(): void {
+    this.updateRoute();
+    this.loadCalls();
+  }
+
+  isPast(call: ICall): boolean {
+    return new Date(call.ended_at) < new Date();
+  }
+}
--- src/app/landing/feature/home/home.component.ts
+++ src/app/landing/feature/home/home.component.ts
@@ -4,10 +4,11 @@ import { SdgsComponent } from '../sdgs/sdgs.component';
 import { AboutComponent } from '../about/about.component';
 import { MissionComponent } from '../mission/mission.component';
 import { WinningSolutionsComponent } from '../winning-solutions/winning-solutions.component';
+import { CallsComponent } from '../calls/calls.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeroComponent, SdgsComponent, AboutComponent, MissionComponent, WinningSolutionsComponent],
+  imports: [HeroComponent, SdgsComponent, AboutComponent, MissionComponent, WinningSolutionsComponent, CallsComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {}
