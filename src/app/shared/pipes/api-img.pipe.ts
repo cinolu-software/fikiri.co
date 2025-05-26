398503227df8f4a70db44d31b3d3acc8643187ef
Wilfried Musanzi <musanziw.m@gmail.com>
2025-05-26 10:06:54 +0200

    fix: improve type handling

--- src/app/shared/pipes/api-img.pipe.ts
+++ src/app/shared/pipes/api-img.pipe.ts
@@ -2,6 +2,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { environment } from '../../../environments/environment';
 
 export type ImageSourceObject = Record<string, string>;
+export interface ImageValue {
+  profile: string;
+  google_image: string;
+  image: string;
+}
 
 @Pipe({
   name: 'apiIMG',
@@ -18,20 +23,23 @@ export class ApiImgPipe implements PipeTransform {
     solution: 'uploads/solutions/',
     call: 'uploads/calls/covers/',
   };
-  transform(value: Record<string, string> | null, key: string): string {
+  transform(value: unknown, key: string): string {
     if (!value) return this.#defaultImages[key] || this.#defaultImages['default'];
     const apiUrl = environment.apiUrl;
+    const typedValue = value as ImageValue;
     switch (key) {
       case 'user':
-        return value['profile']
-          ? `${apiUrl}${this.#imagePaths['user']}${value['profile']}`
-          : value['google_image'] || this.#defaultImages['user'];
+        return typedValue['profile']
+          ? `${apiUrl}${this.#imagePaths['user']}${typedValue['profile']}`
+          : typedValue['google_image'] || this.#defaultImages['user'];
       case 'solution':
-        return value['image']
-          ? `${apiUrl}${this.#imagePaths['solution']}${value['image']}`
+        return typedValue['image']
+          ? `${apiUrl}${this.#imagePaths['solution']}${typedValue['image']}`
           : this.#defaultImages['solution'];
       case 'call':
-        return value['image'] ? `${apiUrl}${this.#imagePaths['call']}${value['image']}` : this.#defaultImages['call'];
+        return typedValue['image']
+          ? `${apiUrl}${this.#imagePaths['call']}${typedValue['image']}`
+          : this.#defaultImages['call'];
       default:
         return this.#defaultImages['default'];
     }
