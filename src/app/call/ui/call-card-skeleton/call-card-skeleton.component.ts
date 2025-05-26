ead3908f2cd3edda4afd415594a42ae82c9384cc
Wilfried Musanzi <musanziw.m@gmail.com>
2025-05-26 15:37:50 +0200

    refactor: remove unused call card and skeleton components

--- src/app/call/ui/call-card-skeleton/call-card-skeleton.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-call-card-skeleton',
-  imports: [],
-  templateUrl: './call-card-skeleton.component.html'
-})
-export class CallCardSkeletonComponent {}
