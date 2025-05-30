2761955ff8ee9ceef612e93272d8418a37336e8d
Wilfried Musanzi <musanziw.m@gmail.com>
2025-05-30 09:40:50 +0200

    refactor: remove server configuration and routing for Angular SSR

--- src/app/app.routes.server.ts
+++
@@ -1,7 +0,0 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
-
-export const serverRoutes: ServerRoute[] = [
-  { path: 'calls', renderMode: RenderMode.Client },
-  { path: 'solutions', renderMode: RenderMode.Client },
-  { path: '**', renderMode: RenderMode.Server },
-];
